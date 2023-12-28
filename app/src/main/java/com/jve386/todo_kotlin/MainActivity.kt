4e106b48708be8ce41033fcab574ed41a27c4413
Jve386 <jve.formacio@gmail.com>
2023-12-28 12:43:38 +0100

    Working on main activity.

--- app/src/main/java/com/jve386/todo_kotlin/MainActivity.kt
+++ app/src/main/java/com/jve386/todo_kotlin/MainActivity.kt
@@ -12,35 +12,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jve386.todo_kotlin.ui.theme.TodokotlinTheme
 
+/* To add in Github's description
+tasks-todo-list in kotlin
+recycler view
+Uso de estilos por contenedor
+
+ */
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TodokotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodokotlinTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
