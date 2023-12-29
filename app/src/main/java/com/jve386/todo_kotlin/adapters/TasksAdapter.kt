e2474384765bfe880f76098865a354224f2b5550
Jve386 <jve.formacio@gmail.com>
2023-12-29 16:48:53 +0100

    Tasks tbd.

--- app/src/main/java/com/jve386/todo_kotlin/adapters/TasksAdapter.kt
+++ app/src/main/java/com/jve386/todo_kotlin/adapters/TasksAdapter.kt
@@ -1,16 +1,19 @@
 package com.jve386.todo_kotlin.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.jve386.todo_kotlin.models.Tags
-
-class TasksAdapter (private val tasks:List<Tags>) : RecyclerView.Adapter<TasksViewHolder>(){
+import com.jve386.todo_kotlin.R
+import com.jve386.todo_kotlin.models.Task
+class TasksAdapter (private val tasks:List<Task>) : RecyclerView.Adapter<TasksViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
-        TODO("Not yet implemented")
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_todo_task, parent, false)
+        return TasksViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.render(tasks[position])
     }
 
     override fun getItemCount() = tasks.size
--- app/src/main/java/com/jve386/todo_kotlin/adapters/TasksViewHolder.kt
+++ app/src/main/java/com/jve386/todo_kotlin/adapters/TasksViewHolder.kt
@@ -2,6 +2,7 @@ package com.jve386.todo_kotlin.adapters
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.jve386.todo_kotlin.models.Tags
 import com.jve386.todo_kotlin.models.Task
 
 class TasksViewHolder (view: View) : RecyclerView.ViewHolder(view) {
