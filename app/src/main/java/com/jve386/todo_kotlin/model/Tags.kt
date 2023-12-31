74d0996319d2ad7d9b949e4e4213bf652868efde
Jve386 <jve.formacio@gmail.com>
2023-12-31 02:52:05 +0100

    todo app done.

--- app/src/main/java/com/jve386/todo_kotlin/model/Tags.kt
+++ app/src/main/java/com/jve386/todo_kotlin/model/Tags.kt
@@ -1,7 +1,7 @@
 package com.jve386.todo_kotlin.model
 
 // Sealed class representing different task tags
-sealed class Tags(var isSelected: Boolean = false) {
+sealed class Tags(var isSelected: Boolean = true) {
     // Object instances for specific tags with default selection state
 
     // Personal tag
